c6d92d5e1429e1a7616dd18bf7d6378f01a89015
Ialgen Allal <ialgen.allal@gmail.com>
2022-02-23 00:30:33 +0100

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,26 @@
-module.exports = {
-  content: [
+export const content = [
     './components/**/*.{vue,js}',
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
-  ],
-  theme: {
+];
+export const theme = {
     extend: {
-      dropShadow: {
-        '3xl': '0 35px 35px rgba(128, 174, 249, 0.25)',
-      },
-      colors: {
-        blue: {
-          edk: '#000366'
+        dropShadow: {
+            '3xl': '0 35px 35px rgba(128, 174, 249, 0.25)'
         },
-        palette: {
-          cyan: '#80D9FF',
-          ciel: '#80AEF9',
-          bleu: '#8083F3',
-          violet: '#9E6EFF',
-          vert: '#C0F0C3'
+        colors: {
+            blue: {
+                edk: '#000366'
+            },
+            palette: {
+                cyan: '#80D9FF',
+                ciel: '#80AEF9',
+                bleu: '#8083F3',
+                violet: '#9E6EFF',
+                vert: '#C0F0C3'
+            }
         }
-      }
     }
-  },
-}
+};
